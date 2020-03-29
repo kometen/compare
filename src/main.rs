e86ea9a468e7e6c3618db20c498c3f4be8b74290
Claus Guttesen <kometen@gmail.com>
2020-03-29 23:32:03 +0200

    Split line and print column.

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() -> Result<(), Error> {
             .multiple(false)
         )
         .arg(Arg::with_name("column")
-            .help("column (one-based indice)")
+            .help("column (zero-based indice)")
             .required(true)
             .takes_value(true)
             .short("c")
@@ -45,31 +45,36 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    let delimiter = matches.value_of("delimiter");
-    println!("delimiter: {}", delimiter.unwrap());
+    let delimiter = matches.value_of("delimiter").unwrap();
 
-    let column = match matches.value_of("column").unwrap().trim().parse::<u8>() {
+    let column = match matches.value_of("column").unwrap().trim().parse::<usize>() {
         Ok(column) => column,
         Err(_e) => { 0 }
     };
-    println!("column: {:?}", column);
 
     for filename in filename_vector {
         println!("Input file: {}", filename);
-        let _result = split_lines(filename.to_string());
+        let _result = split_lines(filename.to_string(), delimiter.to_string(), column);
     }
 
     Ok(())
 }
 
-fn split_lines(f: String) -> Result<(), Error> {
+// https://www.tutorialspoint.com/rust/rust_string.htm
+fn split_lines(f: String, d: String, c: usize) -> Result<(), Error> {
+    println!("delimiter: {}", d);
+    println!("column: {:?}", c);
     let _f = f.clone();
     let input_file = File::open(f);
     let _ = match input_file {
         Ok(input_file) => {
             let reader = BufReader::new(input_file);
             for line in reader.lines() {
-                println!("{}", line?);
+                let s = line.unwrap().to_string();
+                let tokens:Vec<&str> =  s.split(&d).collect();
+                if tokens.len() > c {
+                    println!("{}", tokens[c]);
+                }
             }
         },
         Err(_) => {
